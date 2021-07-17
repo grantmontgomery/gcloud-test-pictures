33940aa654456c4ed09813e15dccf4c86b4a9b3e
Grant <gmontgomery1994@gmail.com>
2021-07-16 18:44:08 -0700

    centered content using display flex.

--- functions/fetchpictures.js
+++ functions/fetchpictures.js
@@ -1,9 +1,10 @@
 exports.getPictures = async (req, res) => {
   try {
-    if (req.method !== "GET") throw new Error("Only get request enabled.");
-    const unsplashPictures = new URL("https://api.unsplash.com/search/photos");
+    // if (req.method !== "GET") throw new Error("Only get request enabled.");
+    // const unsplashPictures = new URL("https://api.unsplash.com/search/photos");
+    // const response = await fetch(unsplashPictures);
 
-    const response = await fetch(unsplashPictures);
+    res.status(200).send("Gcloud test pictures serverless function");
   } catch (error) {
     res.status(500).send(error.message);
   }
